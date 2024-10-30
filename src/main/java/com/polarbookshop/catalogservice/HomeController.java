4f013139503e340b1fa223c51ff9d8ab4a6ff6b4
lee <manarish@naver.com>
2024-10-30 23:41:55 +0900

    update commit

--- src/main/java/com/polarbookshop/catalogservice/HomeController.java
+++ src/main/java/com/polarbookshop/catalogservice/HomeController.java
@@ -6,16 +6,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-	// 사용자 정의 속성 엑세스를 위해 생성자 오토와이어링을 통해 주입된 빈
+
 	private final PolarProperties polarProperties;
 
-	public HomeController(PolarProperties polarProperties){
+	public HomeController(PolarProperties polarProperties) {
 		this.polarProperties = polarProperties;
 	}
 
 	@GetMapping("/")
 	public String getGreeting() {
-		// 설정 데이터 빈에서 가져온 환영 메세지를 사용한다.
 		return polarProperties.getGreeting();
 	}
 
--- src/main/java/com/polarbookshop/catalogservice/domain/Book.java
+++ src/main/java/com/polarbookshop/catalogservice/domain/Book.java
@@ -1,24 +1,24 @@
 package com.polarbookshop.catalogservice.domain;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Positive;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
 
 public record Book (
 
-    @NotBlank(message = "The book ISBN must be defined.")
-    @Pattern(regexp = "^([0-9]{10}|[0-9]{13})$", message = "The ISBN format must be valid.")
-    String isbn,
+        @NotBlank(message = "The book ISBN must be defined.")
+		@Pattern(regexp = "^([0-9]{10}|[0-9]{13})$", message = "The ISBN format must be valid.")
+        String isbn,
 
-    @NotBlank(message = "The book title must be defined.")
-    String title,
+        @NotBlank(message = "The book title must be defined.")
+        String title,
 
-    @NotBlank(message = "The book author must be defined.")
-    String author,
+        @NotBlank(message = "The book author must be defined.")
+        String author,
 
-    @NotNull(message = "The book price must be defined.")
-    @Positive(message = "The book price must be greater than zero.")
-    Double price
-    
+        @NotNull(message = "The book price must be defined.")
+        @Positive(message = "The book price must be greater than zero.")
+        Double price
+
 ){}
