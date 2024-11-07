abe15c417b374ae7e02a5c716574f9ccfc4fefe8
lee <manarish@naver.com>
2024-11-07 23:28:51 +0900

    update commit

--- src/main/java/com/polarbookshop/catalogservice/demo/BookDataLoader.java
+++ src/main/java/com/polarbookshop/catalogservice/demo/BookDataLoader.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 @Profile("testdata")
 public class BookDataLoader {
@@ -20,10 +22,12 @@ public class BookDataLoader {
 
 	@EventListener(ApplicationReadyEvent.class)
 	public void loadBookTestData() {
-		var book1 = new Book("1234567891", "Northern Lights", "Lyra Silverstar", 9.90);
-		var book2 = new Book("1234567892", "Polar Journey", "Iorek Polarson", 12.90);
-		bookRepository.save(book1);
-		bookRepository.save(book2);
+		bookRepository.deleteAll(); // 빈 데이터로 시작하기 위해 기존 책이 있다면 모두 삭제한다.
+
+		var book1 = Book.of("1234567891", "Northern Lights", "Lyra Silverstar", 9.90);
+		var book2 = Book.of("1234567892", "Polar Journey", "Iorek Polarson", 12.90);
+
+		bookRepository.saveAll(List.of(book1, book2)); // 여러 객체를 한 번에 저장.
 	}
 
 }
---
+++ src/test/java/com/polarbookshop/catalogservice/domain/BookRepositoryJdbcTests.java
@@ -0,0 +1,36 @@
+package com.polarbookshop.catalogservice.domain;
+
+import com.polarbookshop.catalogservice.config.DataConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jdbc.core.JdbcAggregateTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
+
+@DataJdbcTest // 스프링 데이터 JDBC 컴포넌트를 집중적으로 테스트하는 클래스임을 나타낸다.
+@Import(DataConfig.class) // 데이터 설정을 임포트한다(감사를 활성화 하기 위해 필요)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) // 테스트컨테이너를 이용해야 하기 때문에 내장 테스트 데이터베이스 사용을 비활성화 한다.
+@ActiveProfiles("integration") // application-integration.yml에서 설정을 로드하기 위해 프로파일을 활성화 한다.
+public class BookRepositoryJdbcTests {
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Autowired
+    private JdbcAggregateTemplate jdbcAggregateTemplate; // 데이터베이스와 상호작용하기 위한 하위 수준의 객체
+
+    @Test
+    void findBookByIsbnWhenExisting(){
+        var bookIsbn = "1234561237";
+        var book = Book.of(bookIsbn, "Title", "Author", 12.90);
+        jdbcAggregateTemplate.insert(book); // JdbcAggregateTemplate은 테스트에 필요한 데이터를 준비하는데 사용한다.
+        Optional<Book> actualBook = bookRepository.findByIsbn(bookIsbn);
+
+        assertThat(actualBook).isPresent();
+        assertThat(actualBook.get().isbn()).isEqualTo(book.isbn());
+    }
+}
